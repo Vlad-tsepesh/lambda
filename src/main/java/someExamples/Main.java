9d48bf26a1aab814d8f7eae4f2eff85cf2c49bf5
Vladtsepesh <vladtsepesh.wow@gmail.com>
2025-04-05 20:17:28 +0300

    added exemaples with 1 and 2 param

--- src/main/java/someExamples/Main.java
+++ src/main/java/someExamples/Main.java
@@ -27,3 +27,41 @@ class LambdaDemo {
 // myNum = () -> "123.03"; // Error!
     }
 }
+
+// Demonstrate a lambda expression that takes a parameter.
+// Another functional interface.
+interface NumericTest {
+    boolean test(int n);
+}
+class LambdaDemo2 {
+    public static void main(String args[])
+    {
+        // A lambda expression that tests if a number is even.
+        NumericTest isEven = (n) -> (n % 2)==0;
+        if(isEven.test(10)) System.out.println("10 is even");
+        if(!isEven.test(9)) System.out.println("9 is not even");
+        // Now, use a lambda expression that tests if a number
+        // is non-negative.
+        NumericTest isNonNeg = (n) -> n >= 0;
+        if(isNonNeg.test(1)) System.out.println("1 is non-negative");
+        if(!isNonNeg.test(-1)) System.out.println("-1 is negative");
+    }
+}
+
+
+// Demonstrate a lambda expression that takes two parameters.
+interface NumericTest2 {
+    boolean test(int n, int d);
+}
+class LambdaDemo3 {
+    public static void main(String args[])
+    {
+        // This lambda expression determines if one number is
+        // a factor of another.
+        NumericTest2 isFactor = (n, d) -> (n % d) == 0;
+        if(isFactor.test(10, 2))
+            System.out.println("2 is a factor of 10");
+        if(!isFactor.test(10, 3))
+            System.out.println("3 is not a factor of 10");
+    }
+}
